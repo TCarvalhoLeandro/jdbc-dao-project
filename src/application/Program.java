042e450a03f0a1d719a661b64beebbc3878c95ed
Leandro <trairicarvalho@gmail.com>
2026-01-18 22:33:34 -0300

    Criacao da entidade seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,31 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Car");
+		SimpleDateFormat stf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			Department obj = new Department(1, "Car");
+			Seller seller = new Seller(1, "Leandro", "leandro@gmail.com", new Date(stf.parse("26/03/1979").getTime()), 
+					5000.0, obj);
+			
+			System.out.println(obj);
+			System.out.println(seller);
+		}
+		catch(ParseException e) {
+			e.printStackTrace();
+		}
 		
-		System.out.println(obj);
+		
+		
 
 	}
 
